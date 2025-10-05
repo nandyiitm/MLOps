42594097df9e0a357b07d86da05d4c968a429a5a
nandy <22f3002857@ds.study.iitm.ac.in>
2025-10-05 22:35:16 +0530

    added files

---
+++ script.py
@@ -0,0 +1,29 @@
+import os
+import joblib
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from pandas.plotting import parallel_coordinates
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn import metrics
+from datetime import datetime
+
+## Simple decision tree model
+data_version = 'v1'
+
+data = pd.read_csv(f'data/{data_version}/data.csv')
+data.head(5)
+
+train, test = train_test_split(data, test_size = 0.4, stratify = data['species'], random_state = 42)
+X_train = train[['sepal_length','sepal_width','petal_length','petal_width']]
+y_train = train.species
+X_test = test[['sepal_length','sepal_width','petal_length','petal_width']]
+y_test = test.species
+
+mod_dt = DecisionTreeClassifier(max_depth = 3, random_state = 1)
+mod_dt.fit(X_train,y_train)
+prediction=mod_dt.predict(X_test)
+print('The accuracy of the Decision Tree is',"{:.3f}".format(metrics.accuracy_score(prediction,y_test)))
+
+joblib.dump(mod_dt, f"model_{data_version}.joblib")
+print(f"Model saved as model_{data_version}.joblib")
